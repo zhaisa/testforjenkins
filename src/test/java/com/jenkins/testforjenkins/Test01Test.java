3c1aed50258dd57b91b4112ee1cf7740b50816d7
Administrator <whatever>
2019-07-09 10:28:39 +0800

    maven run test

--- src/test/java/com/jenkins/testforjenkins/Test01Test.java
+++ src/test/java/com/jenkins/testforjenkins/Test01Test.java
@@ -1,9 +1,51 @@
 package com.jenkins.testforjenkins;
 
+import static org.testng.Assert.assertEquals;
+
+import java.io.File;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 /**
  * Hello world!
  *
  */
 public class Test01Test {
+	public static WebDriver driver;
+	public static String url="http://testhf.irongbei.com";
+@Test(timeOut=4000)
+   public void openBaiDu() {
+//	   driver.findElement(By.linkText("新闻")).click();
+	   try {
+		Thread.sleep(2000);
+	} catch (InterruptedException e) {
+		e.printStackTrace();
+	}
+	   System.out.println(driver.getTitle());
+	
+	  assertEquals("融贝网官网_值得信赖的网络借贷信息中介平台", driver.getTitle());
+	
+   }
+   
+@BeforeClass
+public void startChrome() {
+	File f=new File("src/main/resources/chromedriver.exe");
+	System.setProperty("webdriver.chrome.driver", f.getAbsolutePath());
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("--start-maximized","allow-running-insecure-content","--test-type");
+	options.addArguments("disable-infobars");
+	driver=new ChromeDriver(options);
+	driver.get(url);
+}
+@AfterClass
+public void closeDriver() {
+	driver.close();
+	driver.quit();
+}
 
 }
