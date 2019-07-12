ea7ecb7b8f33c4d2f676a639b6874e884e475106
Administrator <whatever>
2019-07-12 13:43:50 +0800

    ExtentTestNGIReporterListener 生成清晰报告

--- src/main/java/listener/ExtentReport.java
+++ src/main/java/listener/ExtentReport.java
@@ -19,15 +19,15 @@ public class ExtentReport {
 		extent = new ExtentReports(reportLocation, true);// true是覆盖，false是追加
 		extent.addSystemInfo("Host Name", "tiantianliuzehohoho");// 这个是在页面上加了一个“HostName”信息，“tiantianliuzehohoho”是值
 
-		ExtentTest test1 = extent.startTest("Case01", "Sample description");// 用例名
+		ExtentTest test1 = extent.startTest("Test01", "Sample description");// 用例名
 		String image1 = test1.addScreenCapture("../test-output/snapshot/20180227181246995.jpg");// 定义图片的路径（指运行用例过程中根据需要自动保存的截图，一般用于用例失败时的截图），可能图片需要写成变量（后期根据实际项目要作改进）
 		test1.log(LogStatus.INFO, "Running the Case01", image1);// 在页面中添加用例运行信息及图片
 		extent.endTest(test1);
 
-		ExtentTest test2 = extent.startTest("Case02", "Sample description");
-		String image2 = test2.addScreenCapture("../test-output/snapshot/20180130015621085.jpg");
-		test2.log(LogStatus.INFO, "Running the Case02", image2);
-		extent.endTest(test2);
+//		ExtentTest test2 = extent.startTest("Case02", "Sample description");
+//		String image2 = test2.addScreenCapture("../test-output/snapshot/20180130015621085.jpg");
+//		test2.log(LogStatus.INFO, "Running the Case02", image2);
+//		extent.endTest(test2);
 
 		extent.flush();
 		extent.close();
